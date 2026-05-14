9114a194b5f2e67eaf6f3094caee738bec80787f
hmadhsan <hammadhassan616@gmail.com>
2026-05-15 00:08:39 +0200

    remove product hunt badge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -256,11 +256,6 @@ export default async function Home() {
             </div>
           </div>
 
-          {/* Product Hunt Badge */}
-          <div className="mt-16 flex justify-center">
-            <a href="https://www.producthunt.com/products/callio-3/launches/callio-3?embed=true&utm_source=badge-featured&utm_medium=badge&utm_campaign=badge-callio-3" target="_blank" rel="noopener noreferrer">
-              <img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=1083879&theme=light&t=1773491181328" alt="Callio - Connect any API with AI Agent under 1 minute | Product Hunt" width="250" height="54" />
-            </a>
           </div>
         </div>
       </section>
